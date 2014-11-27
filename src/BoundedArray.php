348663db0ef6d746836bf5d5f34e86d95df35fc8
Christian Maioli <mmchristian@gmail.com>
2014-11-27 18:47:58 -0300

    Initial release

---
+++ src/BoundedArray.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace innerspirit\BoundedArray;
+
+class BoundedArray extends ArrayObject
+{
+	public $index;
+
+	public function __construct(array $items, $index = 0)
+	{
+		parent::__construct($items);
+		$this->index = $index;
+	}
+
+	public function next()
+	{
+		if($this->index < $this->count() - 1) {
+			$this->index++;
+		}
+
+		return $this->index;
+	}
+
+	public function prev()
+	{
+		if($this->index > 0) {
+			$this->index--;
+		}
+
+		return $this->index;
+	}
+
+	public function key()
+	{
+		return $this->index;
+	}
+
+}//end class
